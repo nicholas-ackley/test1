6a1691642a2292b041bfb80ff83339a4c1804d6d
nackley2 <nackley2@xpsusa.com>
2024-08-08 12:00:07 -0500

    asd

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import './Project.css'
+import Navbar from '../../Navbar/Navbar'
 const Project = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <div className="projectContainer">
+        <h1>This is my projects page!</h1></div>
     </div>
   )
 }
